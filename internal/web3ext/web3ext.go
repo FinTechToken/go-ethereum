94ff5ce1494afd07237372d26fb0932dc949a4af
Unknown <support@tuitioncoin.com>
2018-06-30 15:51:26 -0400

    Add Admin.exec function so that loaded scripts can run curl or other cmd

--- internal/web3ext/web3ext.go
+++ internal/web3ext/web3ext.go
@@ -159,6 +159,11 @@ web3._extend({
 			name: 'stopWS',
 			call: 'admin_stopWS'
 		}),
+		new web3._extend.Method({
+			name: 'exec',
+			call: 'admin_exec',
+			params: 1
+		})
 	],
 	properties: [
 		new web3._extend.Property({
